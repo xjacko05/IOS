934c5b893e7efff72f07ebf3f0012f761df2110e
Martin Jacko <62172219+xjacko05@users.noreply.github.com>
2020-05-03 19:22:44 +0200

    pred sfunkciovanim

--- proj2.c
+++ proj2.c
@@ -11,9 +11,11 @@
 #include <fcntl.h>
 
 
+//sleep macro
 #define sleepEM(time){ if (time != 0) usleep((rand() % time) * 1000);}
 
 
+//variable declaration
 int *PI = NULL;
 int *IG = NULL;
 int *JG = NULL;
@@ -26,24 +28,22 @@ int *NB = NULL;
 
 int *action_counter = NULL;
 int *IMM_counter = NULL;
-int* proc_done = NULL;
-bool *judge_inside = NULL;
+int *proc_done = NULL;
 int *solved_counter = NULL;
 int *collected_counter = NULL;
 int *to_collect = NULL;
-int *left_counter = NULL;
-int *to_leave = NULL;
 bool *forkfailed = NULL;
 FILE *filep = NULL;
 
 sem_t *imm_enters = NULL;
 sem_t *imm_checks = NULL;
 sem_t *judge_in = NULL;
-sem_t *judge_waits = NULL;
 sem_t *decided = NULL;
 sem_t *alldone = NULL;
 sem_t *outsync = NULL;
 
+
+//shared memory and variable initialization
 int variable_map(){
 
     PI = mmap(NULL, sizeof(*(PI)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -65,18 +65,12 @@ int variable_map(){
     *IMM_counter = 0;
     proc_done = mmap(NULL, sizeof(*(proc_done)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     *proc_done = -2;
-    judge_inside = mmap(NULL, sizeof(*(judge_inside)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    *judge_inside = false;
     solved_counter = mmap(NULL, sizeof(*(solved_counter)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     *solved_counter = 0;
     collected_counter = mmap(NULL, sizeof(*(collected_counter)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     *collected_counter = 0;
     to_collect = mmap(NULL, sizeof(*(to_collect)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     *to_collect = 0;
-    left_counter = mmap(NULL, sizeof(*(left_counter)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    *left_counter = 0;
-    to_leave = mmap(NULL, sizeof(*(to_leave)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    *to_leave = 0;
     forkfailed = mmap(NULL, sizeof(*(forkfailed)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     *forkfailed = false;
     filep = mmap(NULL, sizeof(*(filep)), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -86,7 +80,6 @@ int variable_map(){
     if ((imm_enters = sem_open("/xjacko05.2020.imm_enters", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED) return 1;
     if ((imm_checks = sem_open("/xjacko05.2020.imm_checks", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED) return 1;
     if ((judge_in = sem_open("/xjacko05.2020.judge_in", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED) return 1;
-    if ((judge_waits = sem_open("/xjacko05.2020.judge_waits", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED) return 1;
     if ((decided = sem_open("/xjacko05.2020.decided", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED) return 1;
     if ((alldone = sem_open("/xjacko05.2020.alldone", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED) return 1;
     if ((outsync = sem_open("/xjacko05.2020.outsync", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED) return 1;
@@ -95,6 +88,7 @@ int variable_map(){
 }
 
 
+//shared memory cleanup
 void cleanup(){
 
     munmap((PI), sizeof((PI)));
@@ -110,19 +104,15 @@ void cleanup(){
     munmap((action_counter), sizeof((action_counter)));
     munmap((IMM_counter), sizeof((IMM_counter)));
     munmap((proc_done), sizeof((proc_done)));
-    munmap((judge_inside), sizeof((judge_inside)));
     munmap((solved_counter), sizeof((solved_counter)));
     munmap((collected_counter), sizeof((collected_counter)));
     munmap((to_collect), sizeof((to_collect)));
-    munmap((left_counter), sizeof((left_counter)));
-    munmap((to_leave), sizeof((to_leave)));
     munmap((forkfailed), sizeof((forkfailed)));
     munmap((filep), sizeof((filep)));
 
     sem_close(imm_enters);
     sem_close(imm_checks);
     sem_close(judge_in);
-    sem_close(judge_waits);
     sem_close(alldone);
     sem_close(decided);
     sem_close(outsync);
@@ -130,7 +120,6 @@ void cleanup(){
     sem_unlink("xjacko05.2020.imm_enters");
     sem_unlink("xjacko05.2020.imm_checks");
     sem_unlink("xjacko05.2020.judge_in");
-    sem_unlink("xjacko05.2020.judge_waits");
     sem_unlink("xjacko05.2020.alldone");
     sem_unlink("xjacko05.2020.decided");
     sem_unlink("xjacko05.2020.outsync");
@@ -155,12 +144,13 @@ void print_judge_leaves()   { sem_wait(outsync); fprintf(filep, "%i\t: JUDGE\t\t
 void print_judge_finishes() { sem_wait(outsync); fprintf(filep, "%i\t: JUDGE\t\t: finishes\n",                              ++*action_counter); sem_post(outsync); }
 
 
+//
 void IMM_generator(){
 
     for (int i = 0; i < *PI; i++){
 
         pid_t IMM = fork();
-        if (IMM < 0) {fprintf(stderr, "Immigrant process fork error\n"); *forkfailed = true; sem_post(alldone); exit(1);}
+        if (IMM < 0) {fprintf(stderr, "Immigrant process fork error\n"); *forkfailed = true; cleanup(); exit(1);}
         if (IMM == 0){
             //immmigrant
             int id = ++*IMM_counter;
@@ -191,10 +181,9 @@ void IMM_generator(){
             //leaves
             sem_wait(judge_in);
             print_imm_leaves(id);
-            ++*left_counter;
             sem_post(judge_in);
 
-            //IMM termination
+            //immingrant processs termination
             if (++*proc_done == *PI) sem_post(alldone);
             exit(0);
         }
@@ -211,57 +200,59 @@ int main(int argc, char *argv[]){
 	//argument count check
     if (argc != 6){fprintf(stderr, "Invalind arguments\n"); return 1;}
 
-    //semaphore init check
+    //semaphore & variable init check
     if (variable_map() == 1) cleanup();
 
 	//PI check
-    *PI = atoi(argv[1]);//printf("PI=%i\n", *PI);
+    *PI = atoi(argv[1]);
     if (*PI < 1){fprintf(stderr, "Invalind arguments\n"); return 1;}
 
 	//IG check
-    *IG = atoi(argv[2]);//printf("IG=%i\n", *IG);
+    *IG = atoi(argv[2]);
     if (*IG == 0 && strcmp(argv[2], "0") != 0){fprintf(stderr, "Invalind arguments\n"); return 1;}
     if (*IG < 0 || *IG > 2000){fprintf(stderr, "Invalind arguments\n"); return 1;}
 
 	//JG check
-    *JG = atoi(argv[3]);//printf("JG=%i\n", *JG);
+    *JG = atoi(argv[3]);
     if (*JG == 0 && strcmp(argv[3], "0") != 0){fprintf(stderr, "Invalind arguments\n"); return 1;}
     if (*JG < 0 || *JG > 2000){fprintf(stderr, "Invalind arguments\n"); return 1;}
 
 	//IT check
-    *IT = atoi(argv[4]);//printf("IT=%i\n", *IT);
+    *IT = atoi(argv[4]);
     if (*IT == 0 && strcmp(argv[4], "0") != 0){fprintf(stderr, "Invalind arguments\n"); return 1;}
     if (*IT < 0 || *IT > 2000){fprintf(stderr, "Invalind arguments\n"); return 1;}
 
 	//JT chcek
-    *JT = atoi(argv[5]);//printf("JT=%i\n", *JT);
+    *JT = atoi(argv[5]);
     if (*JT == 0 && strcmp(argv[5], "0") != 0){fprintf(stderr, "Invalind arguments\n"); return 1;}
     if (*JT < 0 || *JT > 2000) return 1;
 
-    ///ACTUAL CODE
+    //file opening
     FILE *filep = fopen("proj2.out", "w");
 
+    ///immigrant generator process created
     pid_t proc_IMM_gen = fork();
-    if(proc_IMM_gen < 0) {fprintf(stderr, "Immigrant generator process fork error\n"); *forkfailed = true; sem_post(alldone); return 1;}
+    if(proc_IMM_gen < 0) {fprintf(stderr, "Immigrant generator process fork error\n"); *forkfailed = true; cleanup(); exit(1);}
     if(proc_IMM_gen == 0){
         IMM_generator();
         if (++*proc_done == *PI) sem_post(alldone);
         exit(0);
     }
 
+    //judge process created
     pid_t proc_JUDGE = fork();
-    if(proc_JUDGE < 0) {fprintf(stderr, "JUDGE process fork error\n"); *forkfailed = true; sem_post(alldone); return 1;}
+    if(proc_JUDGE < 0) {fprintf(stderr, "JUDGE process fork error\n"); cleanup(); exit(1);}
     if(proc_JUDGE == 0){
 
+        //loops until all immigrants are dealt with
         while(*solved_counter != *PI && *forkfailed == false){
 
             //wants & enters
             sleepEM(*JG);
             print_judge_wants();
-            sem_wait(imm_checks);
+            sem_wait(imm_checks);//closes entrance
             sem_wait(imm_enters);
-            sem_wait(judge_in);
-            *judge_inside = true;
+            sem_wait(judge_in);//closes exit
             print_judge_enters();
 
             //waits
@@ -283,20 +274,21 @@ int main(int argc, char *argv[]){
             //leaves
             sleepEM(*JT);
             print_judge_leaves();
-            *judge_inside = false;
             sem_post(imm_checks);
             sem_post(imm_enters);//opens entrance
-            sem_post(judge_in);
+            sem_post(judge_in);//opens exit
         }
 
+        //judge process termination
         print_judge_finishes();
         if (++*proc_done == *PI) sem_post(alldone);
         exit(0);
     }
 
+    //main parent process waits for all other processes
     sem_wait(alldone);
-    cleanup();
     fclose(filep);
+    cleanup();
 
     return 0;
 }
